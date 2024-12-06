331e694115a57d18e1ead191bfd0cd014aee5a81
Niclas Olmenius <niclas@voysys.se>
2024-12-06 11:49:24 +0100

    part 2

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -99,7 +99,121 @@ pub fn part_one() -> i32 {
 }
 
 pub fn part_two() -> i32 {
-    let input = include_str!("../input.txt");
+    let input = include_str!("../input.txt").split('\n').collect::<String>();
+    let input = input.as_bytes();
 
-    0
+    const GRID_SIDE: i32 = 130;
+
+    #[derive(Clone, Copy, PartialEq)]
+    enum GuardDirection {
+        Up,
+        Right,
+        Down,
+        Left,
+    }
+
+    struct Grid<'a> {
+        data: &'a [u8],
+        direction: GuardDirection,
+        visited_map: Vec<(u8, Vec<GuardDirection>)>,
+        x: i32,
+        y: i32,
+    }
+
+    fn index(x: i32, y: i32) -> usize {
+        (x + GRID_SIDE * y) as usize
+    }
+
+    impl<'a> Grid<'a> {
+        fn new(data: &'a [u8]) -> Self {
+            let guard_index = data.iter().position(|x| *x == b'^').unwrap() as i32;
+
+            let x = guard_index % GRID_SIDE;
+            let y = guard_index / GRID_SIDE;
+
+            let mut visited_map = Vec::new();
+            visited_map.resize((GRID_SIDE * GRID_SIDE) as usize, (0, Vec::new()));
+            visited_map[index(x, y)] = (1, vec![GuardDirection::Up]);
+
+            Self {
+                data,
+                direction: GuardDirection::Up,
+                visited_map,
+                x,
+                y,
+            }
+        }
+
+        fn walking_in_loop(&mut self) -> bool {
+            let next_coord = match self.direction {
+                GuardDirection::Up => (self.x, self.y - 1),
+                GuardDirection::Right => (self.x + 1, self.y),
+                GuardDirection::Down => (self.x, self.y + 1),
+                GuardDirection::Left => (self.x - 1, self.y),
+            };
+
+            // outside
+            if next_coord.0 > (GRID_SIDE - 1)
+                || next_coord.0 < 0
+                || next_coord.1 < 0
+                || next_coord.1 > (GRID_SIDE - 1)
+            {
+                return false;
+            }
+
+            let idx = index(next_coord.0, next_coord.1);
+            let cell = self.data[idx];
+
+            match cell {
+                b'.' | b'^' => {
+                    // already visited in this direction, loop
+                    if self.visited_map[idx].0 == 1
+                        && self.visited_map[idx].1.contains(&self.direction)
+                    {
+                        return true;
+                    }
+
+                    self.visited_map[idx].0 = 1;
+                    self.visited_map[idx].1.push(self.direction);
+
+                    self.x = next_coord.0;
+                    self.y = next_coord.1;
+                }
+                b'#' => {
+                    match self.direction {
+                        GuardDirection::Up => self.direction = GuardDirection::Right,
+                        GuardDirection::Right => self.direction = GuardDirection::Down,
+                        GuardDirection::Down => self.direction = GuardDirection::Left,
+                        GuardDirection::Left => self.direction = GuardDirection::Up,
+                    }
+                    if self.visited_map[idx].0 == 1
+                        && self.visited_map[idx].1.contains(&self.direction)
+                    {
+                        return true;
+                    }
+                }
+                _ => (),
+            }
+
+            self.walking_in_loop()
+        }
+    }
+
+    let mut obstruction_positions = 0;
+
+    for i in 0..input.len() {
+        if input[i] == b'#' || input[i] == b'^' {
+            continue;
+        }
+        let mut input = input.to_owned();
+        input[i] = b'#';
+
+        let mut grid = Grid::new(&input);
+
+        if grid.walking_in_loop() {
+            obstruction_positions += 1;
+        }
+    }
+
+    obstruction_positions
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,11 +4,11 @@ use day06::part_one;
 use day06::part_two;
 
 fn main() {
-    let start = Instant::now();
-    let out = part_one();
-    let elapsed = start.elapsed().as_micros();
+    // let start = Instant::now();
+    // let out = part_one();
+    // let elapsed = start.elapsed().as_micros();
 
-    println!("Part One: {out}, time: {elapsed} us");
+    // println!("Part One: {out}, time: {elapsed} us");
 
     let start = Instant::now();
     let out = part_two();
