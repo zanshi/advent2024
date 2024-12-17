0e2485af2a81d81684f23fa24cd62e983161d0cd
Niclas Olmenius <niclas@voysys.se>
2024-12-17 16:34:52 +0100

    wip 17b

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -1,13 +1,13 @@
-use std::fmt::Write;
+use std::{fmt::Write, i64};
 enum Opcode {
     Adv = 0,
-    Bxl,
-    Bst,
-    Jnz,
-    Bxc,
-    Out,
-    Bdv,
-    Cdv,
+    Bxl = 1,
+    Bst = 2,
+    Jnz = 3,
+    Bxc = 4,
+    Out = 5,
+    Bdv = 6,
+    Cdv = 7,
 }
 
 impl From<u8> for Opcode {
@@ -90,6 +90,8 @@ enum Error {
 }
 
 fn run_program(input: &Input, check: bool) -> Result<Vec<u8>, Error> {
+    let initial_a = input.reg_a;
+
     let mut reg_a = input.reg_a;
     let mut reg_b = input.reg_b;
     let mut reg_c = input.reg_c;
@@ -97,32 +99,29 @@ fn run_program(input: &Input, check: bool) -> Result<Vec<u8>, Error> {
 
     let mut instruction_ptr = 0;
 
+    let mut op_sequence: Vec<u8> = Vec::new();
+    let mut saved_a: Vec<i64> = Vec::new();
+    let mut saved_b: Vec<i64> = Vec::new();
+    let mut saved_c: Vec<i64> = Vec::new();
+    let mut saved_operands: Vec<u8> = Vec::new();
     let mut output: Vec<u8> = Vec::new();
 
     while instruction_ptr < (program.len() - 1) {
         let opcode = Opcode::from(program[instruction_ptr]);
         let operand = program[instruction_ptr + 1];
 
+        op_sequence.push(program[instruction_ptr]);
+        op_sequence.push(program[instruction_ptr + 1]);
+
         match opcode {
             Opcode::Adv => {
-                let combo = combo_operand(operand, reg_a, reg_b, reg_c);
-
-                let num = reg_a;
-                let denum = 2i64.pow(combo as u32);
-
-                let res = num / denum;
-
-                reg_a = res;
+                reg_a >>= combo_operand(operand, reg_a, reg_b, reg_c);
             }
             Opcode::Bxl => {
-                let res = reg_b ^ operand as i64;
-
-                reg_b = res;
+                reg_b ^= operand as i64;
             }
             Opcode::Bst => {
-                let res = combo_operand(operand, reg_a, reg_b, reg_c) % 8;
-
-                reg_b = res;
+                reg_b = combo_operand(operand, reg_a, reg_b, reg_c) & 0b111;
             }
             Opcode::Jnz => {
                 if reg_a != 0 {
@@ -140,12 +139,37 @@ fn run_program(input: &Input, check: bool) -> Result<Vec<u8>, Error> {
                 reg_b = res;
             }
             Opcode::Out => {
-                let val = combo_operand(operand, reg_a, reg_b, reg_c) % 8;
+                let val = combo_operand(operand, reg_a, reg_b, reg_c) & 0b111;
                 output.push(val as u8);
 
                 if check {
                     if output != input.program[..output.len()] {
                         return Err(Error::Wrong);
+                    }
+
+                    saved_a.push(reg_a);
+                    saved_b.push(reg_b);
+                    saved_c.push(reg_c);
+                    saved_operands.push(operand);
+
+                    if output.len() == 9 {
+                        println!("Op sequence: {:?}", op_sequence);
+                        println!("Initial: {:016} ({:048b})", initial_a, initial_a);
+
+                        for i in 0..output.len() {
+                            let out = output[i];
+                            let a = saved_a[i];
+                            let b = saved_b[i];
+                            let c = saved_c[i];
+                            let op = saved_operands[i];
+
+                            println!(
+                                "output: {} ({:03b}), operand: {} ({:03b}) , a: {:016} ({:048b}), b: {:016} ({:048b}), a ^ b: ({:048b}), c: ({:048b})",
+                                out, out, op, op, a, a, b, b, a ^ b, c
+                            )
+                        }
+
+                        println!();
                     }
 
                     if output.len() == input.program.len() {
@@ -155,23 +179,11 @@ fn run_program(input: &Input, check: bool) -> Result<Vec<u8>, Error> {
             }
             Opcode::Bdv => {
                 let combo = combo_operand(operand, reg_a, reg_b, reg_c);
-
-                let num = reg_a;
-                let denum = 2i64.pow(combo as _);
-
-                let res = num / denum;
-
-                reg_b = res;
+                reg_b = reg_a >> combo;
             }
             Opcode::Cdv => {
                 let combo = combo_operand(operand, reg_a, reg_b, reg_c);
-
-                let num = reg_a;
-                let denum = 2i64.pow(combo as _);
-
-                let res = num / denum;
-
-                reg_c = res;
+                reg_c = reg_a >> combo;
             }
         }
 
@@ -200,9 +212,28 @@ pub fn part_one(input: &str) -> String {
     output_string
 }
 
+fn bitsearch() {}
+
 pub fn part_two(input: &str) -> i64 {
     let mut input = parse_input(input);
     input.reg_a = 0;
+
+    let mut output_val = 0i64;
+
+    for (i, val) in input.program.iter().enumerate() {
+        output_val |= (val & 0b111) as i64;
+        output_val <<= 3;
+    }
+
+    output_val >>= 3;
+
+    println!("Program: ");
+    for val in input.program.iter().rev() {
+        print!("{:03b}", *val);
+    }
+    println!();
+
+    println!("Program: {:048b}", output_val);
 
     loop {
         input.reg_a += 1;
